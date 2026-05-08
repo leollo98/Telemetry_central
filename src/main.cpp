381a036ca58c68b0f5a1a8bdf61490d8cbf745e0
leollo98 <leo.sartorello98@gmail.com>
2026-05-08 01:55:10 -0300

    add error HTML

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include <credenciais.h>
 #include <ledHTML.h>
 #include <prometheusHTML.h>
+#include <erroHTML.h>
 
 // tft
 #include <Adafruit_GFX.h>
@@ -667,30 +668,7 @@ String SendEcolhaAlarmeHTML() {
 }
 
 String SendERRORHTML() {
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +=
-      "<head><meta name=\"viewport\" charset= \"utf-8\" "
-      "content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<title>Sensors Control</title>\n";
-  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: "
-         "0px auto; text-align: center;}\n";
-  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} "
-         "h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr +=
-      ".button {display: block;width: 80px;background-color: #3498db;border: "
-      "none;color: white;padding: 16px 32px;text-decoration: none;font-size: "
-      "100px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr += ".button-on {background-color: #3498db;}\n";
-  ptr += ".button-on:active {background-color: #2980b9;}\n";
-  ptr += ".button-off {background-color: #34495e;}\n";
-  ptr += ".button-off:active {background-color: #2c3e50;}\n";
-  ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr += "</style>\n";
-  ptr += "</head>\n";
-  ptr += "<body>\n";
-  ptr += "<h1>ESP32 Web Server</h1>\n";
-  ptr += "<h3>Parametro fora de limite:</h3>\n";
-  return ptr;
+  return ERROR_HTML;
 }
 
 void handle_OnConnect(AsyncWebServerRequest *request) {
